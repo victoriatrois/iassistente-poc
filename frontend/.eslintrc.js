af37999fb653df4ebba9593864e5290ad1b4146c
victoriatrois <victoriatrois@outlook.com>
2026-07-03 01:03:17 -0300

    ci: make eslint work properly

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
-  root: true,
   extends: '@react-native',
+  ignorePatterns: [
+    'node_modules',
+    'Pods',
+    'android',
+    'ios',
+    'dist',
+    'build',
+    '.git',
+  ],
 };
